1de120b68ee3a521cea12791d323f4d76dfdafef
Sali_dk52@itstepru.onmicrosoft.com <Sali_dk52@itstepru.onmicrosoft.com>
2022-03-17 19:45:21 +0300

    Добавьте файлы проекта.

---
+++ homework1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework1
+{
+    public partial class Form1 : Form
+    {
+        string result;
+        double a;
+        double b;
+        double c;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            result = textBox1.Text;
+            a = Convert.ToDouble(result);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            result = textBox1.Text;
+            b = Convert.ToDouble(result);
+        }
+
+        private void textBox3_MouseClick(object sender, MouseEventArgs e)
+        {
+            c = a + b;
+            textBox3.Text = Convert.ToString(c);
+        }
+    }
+}
